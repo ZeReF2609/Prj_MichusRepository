a946c4c6295ab86d3d1f0b1d64e96f964ccb874b
Yael1815 <yamilealmanza.18@gmail.com>
2024-11-11 12:37:15 -0500

    agregando el viewModel del dashboard

---
+++ Michus/Models/DashboardViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+
+namespace Michus.Models
+{
+    public class DashboardViewModel
+    {
+        public int TotalSales { get; set; }
+        public int TotalProducts { get; set; }
+        public int TotalClients { get; set; }
+        public List<CategoryProductCount> ProductCountByCategory { get; set; }
+        public List<CategoryProductValue> ProductValueByCategory { get; set; }
+    }
+
+    public class CategoryProductCount
+    {
+        public string Category { get; set; }
+        public int ProductCount { get; set; }
+    }
+
+    public class CategoryProductValue
+    {
+        public string Category { get; set; }
+        public decimal TotalValue { get; set; }
+    }
+}
