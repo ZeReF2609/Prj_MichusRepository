f56e410d296446a751eeed6d0cccc56f8d75026a
MauriciOjeda <mauricioojeda02@gmail.com>
2024-11-19 07:42:47 -0500

    Correccion del archivo de Modelo anio

---
+++ Michus/Models/pa_lista_descuento.cs
@@ -0,0 +1,18 @@
+﻿namespace Michus.Models
+{
+    public class pa_lista_descuento
+    {
+        public string IdDescuento { get; set; } = null!;
+        public int? IdPromocion { get; set; }
+        public string? IdEvento { get; set; }
+        public DateTime FechaInicio { get; set; }
+        public DateTime FechaFin { get; set; }
+        public decimal PrecioDescuento { get; set; }
+        public byte TipoDescuento { get; set; }
+        public string? IdCategoria { get; set; }
+        public List<string>? IdArticulos { get; set; } = null;
+        public int Estado { get; set; }
+        public string? TiSitu { get; set; }
+       
+    }
+}
