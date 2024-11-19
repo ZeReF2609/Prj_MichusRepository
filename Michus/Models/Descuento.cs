518a67921b8ac918c42ae1737298c3c1d8fa03e6
MauriciOjeda <mauricioojeda0204@gmail.com>
2024-11-19 09:31:36 -0500

    Create Descuento.cs
    arreglando descuento

---
+++ Michus/Models/Descuento.cs
@@ -0,0 +1,16 @@
+﻿namespace Michus.Models
+{
+    public class Descuento
+    {
+        public string? IdDescuento { get; set; }
+        public int? IdPromocion { get; set; }
+        public string? IdEvento { get; set; }
+        public DateTime FechaInicio { get; set; }
+        public DateTime FechaFin { get; set; }
+        public decimal PrecioDescuento { get; set; }
+        public byte TipoDescuento { get; set; } // 0 = Fijo, 1 = Porcentual
+        public bool AplicarCategoria { get; set; }
+        public string? IdCategoria { get; set; }
+        public string? IdArticulos { get; set; }
+    }
+}
