6ceb909865445a1683dc2a069407a303bd68b677
Anyelo Alexander Huaman Huarcaya <I202030342@cibertec.edu.pe>
2024-11-25 23:40:44 -0500

    Recuperando cambios

--- Michus/obj/Debug/net8.0/Michus.AssemblyInfo.cs
+++ Michus/obj/Debug/net8.0/Michus.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Michus")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+52ea41c45c63ed70a00ba6956fccd9654ca0bd47")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+af1eae7649dd214f873fdee0319cb97d60d2c323")]
 [assembly: System.Reflection.AssemblyProductAttribute("Michus")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Michus")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
