3c09eff391a2c745cc7cf52ebf29cdfc3c1c16e3
Yael1815 <150535861+Yael1815@users.noreply.github.com>
2024-11-26 12:35:34 -0500

    Update MenuController.cs

--- Michus/Controllers/MenuController.cs
+++ Michus/Controllers/MenuController.cs
@@ -1,9 +1,16 @@
-﻿using Michus.Service;
+using Michus.Service;
+using Michus.Models;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using System.Data;
 using System.Diagnostics;
 using System.Security.Claims;
 using System.Text.Json;
+using System.Threading.Tasks;
+using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 
 namespace Michus.Controllers
@@ -12,21 +19,82 @@ namespace Michus.Controllers
     public class MenuController : Controller
     {
         private readonly MenuService _menuService;
-        
-        public MenuController(MenuService menuService)
+
+        private readonly IConfiguration _configuration;
+        public MenuController(MenuService menuService, IConfiguration configuration)
         {
+            _configuration = configuration;
             _menuService = menuService;
         }
 
         public async Task<IActionResult> Index()
         {
-            // Configura encabezados para evitar el almacenamiento en caché
+            var dashboardViewModel = new DashboardViewModel();
+
+            await using (var connection = new SqlConnection(_configuration.GetConnectionString("cn1")))
+            {
+                await connection.OpenAsync();
+
+                // Get total sales
+                await using (var command = new SqlCommand("SP_GetTotalSales", connection))
+                {
+                    command.CommandType = CommandType.StoredProcedure;
+                    dashboardViewModel.TotalSales = Convert.ToInt32(await command.ExecuteScalarAsync());
+                }
+
+                // Get total products
+                await using (var command = new SqlCommand("SP_GetTotalProducts", connection))
+                {
+                    command.CommandType = CommandType.StoredProcedure;
+                    dashboardViewModel.TotalProducts = Convert.ToInt32(await command.ExecuteScalarAsync());
+                }
+
+                // Get total clients
+                await using (var command = new SqlCommand("SP_GetTotalClients", connection))
+                {
+                    command.CommandType = CommandType.StoredProcedure;
+                    dashboardViewModel.TotalClients = Convert.ToInt32(await command.ExecuteScalarAsync());
+                }
+
+                // Get product count by category
+                dashboardViewModel.ProductCountByCategory = new List<CategoryProductCount>();
+                await using (var command = new SqlCommand("SP_GetProductCountByCategory", connection))
+                {
+                    command.CommandType = CommandType.StoredProcedure;
+                    using var reader = await command.ExecuteReaderAsync();
+                    while (await reader.ReadAsync())
+                    {
+                        dashboardViewModel.ProductCountByCategory.Add(new CategoryProductCount
+                        {
+                            Category = reader["CATEGORIA"].ToString(),
+                            ProductCount = Convert.ToInt32(reader["ProductCount"])
+                        });
+                    }
+                }
+
+                // Get total product value by category
+                dashboardViewModel.ProductValueByCategory = new List<CategoryProductValue>();
+                await using (var command = new SqlCommand("SP_GetTotalProductValueByCategory", connection))
+                {
+                    command.CommandType = CommandType.StoredProcedure;
+                    using var reader = await command.ExecuteReaderAsync();
+                    while (await reader.ReadAsync())
+                    {
+                        dashboardViewModel.ProductValueByCategory.Add(new CategoryProductValue
+                        {
+                            Category = reader["CATEGORIA"].ToString(),
+                            TotalValue = Convert.ToDecimal(reader["TotalValue"])
+                        });
+                    }
+                }
+            }
             SetNoCacheHeaders();
 
             // Carga los datos del menú según el rol del usuario actual
             await LoadMenuDataAsync();
-            return View();
+            return View(dashboardViewModel);
         }
+
 
         private string GetCurrentRoleId()
         {
