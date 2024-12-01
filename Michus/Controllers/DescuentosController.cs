a145c908532c5638f622ffe123ec2fa512bdca90
MauriciOjeda <mauricioojeda0204@gmail.com>
2024-12-01 03:23:12 -0500

    Update DescuentosController.cs
    Actualización de Controller para registrar

--- Michus/Controllers/DescuentosController.cs
+++ Michus/Controllers/DescuentosController.cs
@@ -1,4 +1,4 @@
-﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Michus.Service;
 using Michus.DAO;
@@ -6,13 +6,16 @@ using System.Threading.Tasks;
 using System.Security.Claims;
 using Michus.Models;
 using Microsoft.AspNetCore.Mvc.Rendering;
+using Newtonsoft.Json.Linq;
 using System.Data.SqlClient;
 using System;
 using System.Data;
 using System.Dynamic;
+using System.Text.Json;
 
 namespace Michus.Controllers
 {
+    [Route("Descuentos")]
     [Authorize]
     public class DescuentosController : Controller
     {
@@ -27,33 +30,35 @@ namespace Michus.Controllers
             _descuentosDAO = new DescuentosDAO(_connectionString);
         }
 
-
-
-        public async Task<ActionResult> listadescuentos(int? FECHA_INICIO = null, int? FECHA_FIN = null, string? TI_SITU = "PAP")
+        // Ruta general para la lista de descuentos
+        [HttpGet("ListaDescuentos")]
+        public async Task<ActionResult> Listadescuentos(int? FECHA_INICIO = null, int? FECHA_FIN = null, string? TI_SITU = "PAP")
         {
+            // Cargar datos del menú
             await LoadMenuDataAsync();
 
+            // Obtener años de inicio y fin para los descuentos
             var aniosInicioDesc = await _descuentosDAO.GetAnioInicioDesc();
             var aniosFinDesc = await _descuentosDAO.GetAnioFinDesc();
 
+            // Asignar a ViewBag para los select lists
             ViewBag.AniosInicioDesc = new SelectList(aniosInicioDesc, "anio", "anio");
             ViewBag.AniosFinDesc = new SelectList(aniosFinDesc, "anio", "anio");
 
-
+            // Asignar valores de los filtros al ViewBag
             ViewBag.TiSitu = TI_SITU;
             ViewBag.FechaInicio = FECHA_INICIO;
             ViewBag.FechaFin = FECHA_FIN;
 
-            var descuentos = await _descuentosDAO.GetDescuentosCartilla(FECHA_INICIO, FECHA_FIN, TI_SITU!);
+            // Obtener los descuentos con los filtros aplicados
+            var descuentos = await _descuentosDAO.GetDescuentosCartilla(FECHA_INICIO, FECHA_FIN, TI_SITU);
 
-
-            ViewBag.TiSitu = TI_SITU; 
-
+            // Retornar la vista con los descuentos
             return View(descuentos);
         }
 
-
-        [HttpGet]
+        // Ruta para obtener los detalles del descuento
+        [HttpGet("DetallesDescuento/{id}")]
         public async Task<IActionResult> GetDetallesDescuento(string id)
         {
             if (id == null)
@@ -112,9 +117,8 @@ namespace Michus.Controllers
             }
         }
 
-
-
-        [HttpPost]
+        // Ruta para actualizar el estado del descuento
+        [HttpPost("ActualizarSitu")]
         public IActionResult ActualizarSitu()
         {
             string idDescuento = Request.Form["idDescuento"];
@@ -145,6 +149,108 @@ namespace Michus.Controllers
 
 
 
+        [HttpPost("RegistrarDescuento")]
+        public async Task<IActionResult> RegistrarDescuento([FromBody] JsonElement request)
+        {
+            try
+            {
+                // Obtención de las propiedades del JSON
+                var fechaInicio = request.GetProperty("FECHA_INICIO").ToString();
+                var fechaFin = request.GetProperty("FECHA_FIN").ToString();
+                var precioDescuento = request.GetProperty("PRECIO_DESCUENTO").ToString();
+                var tipoDescuento = request.GetProperty("TIPO_DESCUENTO").ToString();
+                var aplicarCategoria = request.GetProperty("APLICAR_CATEGORIA").ToString();
+                var idCategorias = request.GetProperty("ID_CATEGORIA");
+                var idArticulos = request.GetProperty("ID_ARTICULOS");
+
+                // Variables para convertir las fechas
+                DateTime fechaInicioDateTime;
+                DateTime fechaFinDateTime;
+
+                // Verificación de la fecha de inicio
+                if (string.IsNullOrWhiteSpace(fechaInicio) || !DateTime.TryParse(fechaInicio, out fechaInicioDateTime))
+                {
+                    return BadRequest("El formato de la fecha de inicio es inválido.");
+                }
+
+                // Verificación de la fecha de fin
+                if (string.IsNullOrWhiteSpace(fechaFin) || !DateTime.TryParse(fechaFin, out fechaFinDateTime))
+                {
+                    return BadRequest("El formato de la fecha de fin es inválido.");
+                }
+
+                // Verificación de 'precioDescuento'
+                if (string.IsNullOrWhiteSpace(precioDescuento) || !decimal.TryParse(precioDescuento, out decimal result))
+                {
+                    return BadRequest("El campo 'PrecioDescuento' es inválido.");
+                }
+
+                // Verificación de 'tipoDescuento'
+                if (string.IsNullOrEmpty(tipoDescuento))
+                {
+                    return BadRequest("El campo 'TipoDescuento' es obligatorio.");
+                }
+
+                // Validación de la categoría
+                if (aplicarCategoria != null && (aplicarCategoria == "categoria" && idCategorias.ValueKind == JsonValueKind.Null))
+                {
+                    return BadRequest("Se debe especificar al menos una categoría si el tipo de descuento es 'categoria'.");
+                }
+
+                // Validación de artículos
+                if (aplicarCategoria != null && (aplicarCategoria == "articulo" && idArticulos.ValueKind == JsonValueKind.Null))
+                {
+                    return BadRequest("Se debe especificar al menos un artículo si el tipo de descuento es 'articulo'.");
+                }
+
+                // Convertir 'idCategorias' y 'idArticulos' a listas
+                var idArticulosList = new List<string>();
+                var idCategoriasList = new List<string>();
+
+
+                // Si 'idCategorias' es una cadena separada por comas
+                if (idCategorias.ValueKind == JsonValueKind.String)
+                {
+                    idCategoriasList = idCategorias.GetString().Split(',').ToList();
+                }
+
+                // Si 'idArticulos' es una cadena separada por comas
+                if (idArticulos.ValueKind == JsonValueKind.String)
+                {
+                    idArticulosList = idArticulos.GetString().Split(',').ToList();
+                }
+
+                // Conexión a la base de datos y ejecución del procedimiento almacenado
+                using (var connection = new SqlConnection(_connectionString))
+                {
+                    await connection.OpenAsync();
+
+                    using (var command = new SqlCommand("SP_CREAR_DESCUENTO", connection))
+                    {
+                        command.CommandType = CommandType.StoredProcedure;
+
+                        // Parámetros para el procedimiento almacenado
+                        command.Parameters.AddWithValue("@FECHA_INICIO", fechaInicioDateTime);
+                        command.Parameters.AddWithValue("@FECHA_FIN", fechaFinDateTime);
+                        command.Parameters.AddWithValue("@PRECIO_DESCUENTO", precioDescuento);
+                        command.Parameters.AddWithValue("@TIPO_DESCUENTO", tipoDescuento);
+                        command.Parameters.AddWithValue("@APLICAR_CATEGORIA", aplicarCategoria);
+                        command.Parameters.AddWithValue("@ID_CATEGORIA", idCategoriasList.Any() ? string.Join(",", idCategoriasList) : DBNull.Value);
+                        command.Parameters.AddWithValue("@ID_ARTICULOS", idArticulosList.Any() ? string.Join(",", idArticulosList) : DBNull.Value);
+
+                        // Ejecutar el procedimiento almacenado
+                        await command.ExecuteNonQueryAsync();
+                    }
+
+                    // Responder con un mensaje de éxito
+                    return NoContent();
+                }
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, $"Error interno del servidor: {ex.Message}");
+            }
+        }
 
 
 
@@ -156,6 +262,22 @@ namespace Michus.Controllers
 
 
 
+
+        // Ruta para listar productos
+        [HttpGet("ListarProductos")]
+        public async Task<IActionResult> ListarProductos()
+        {
+            var productos = await _descuentosDAO.ListarProductosAsync();
+            return Ok(productos);
+        }
+
+        // Ruta para listar categorías
+        [HttpGet("ListarCategorias")]
+        public async Task<IActionResult> ListarCategorias()
+        {
+            var categoria = await _descuentosDAO.ListarCategoriasAsync();
+            return Ok(categoria);
+        }
 
         // Cargar datos de menú
         private async Task LoadMenuDataAsync()
