543fdb99832a7ea2928a34d8ab015521508baa81
Anyelo Alexander Huaman Huarcaya <I202030342@cibertec.edu.pe>
2024-12-03 11:06:50 -0500

    Agregando el Modelo ventaDetalle

---
+++ Michus/Models/VentaDetalle.cs
@@ -0,0 +1,10 @@
+﻿namespace Michus.Models
+{
+    public class VentaDetalle
+    {
+        public string IdProducto { get; set; }
+        public string NombreProducto { get; set; }
+        public int Cantidad { get; set; }
+        public decimal PrecioUnitario { get; set; }
+    }
+}
